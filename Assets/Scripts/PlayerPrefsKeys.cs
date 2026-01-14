b235723c2c56a52675bd2839b196361e618e5b83
Abdullah Akyol <abdullah35akyol@gmail.com>
2026-01-14 23:17:34 +0300

    REFACTOR: PlayerPrefsKeys + ButtonController + OfflineRewardConfig

--- Assets/Scripts/PlayerPrefsKeys.cs
+++ Assets/Scripts/PlayerPrefsKeys.cs
@@ -1,6 +1,7 @@
 
 public static class PlayerPrefsKeys
 {
-    public const string LAST_EXIT_TIME = "OfflineReward_LastExitTime";
-    public const string HAS_SAVED_DATA = "OfflineReward_HasSavedData";
+    public const string LAST_EXIT_TIME = "OfflineReward_LastExitTime"; //son cikis zamani
+    public const string HAS_SAVED_DATA = "OfflineReward_HasSavedData"; //kayitli veri var mi
+    public const string ACCUMULATED_DURATION_TICKS = "OfflineReward_AccumulatedDurationTicks"; //collect edilene kadar offline + online
 }
--- Assets/Scripts/UI Scripts/ButtonController.cs
+++ Assets/Scripts/UI Scripts/ButtonController.cs
@@ -1,28 +1,43 @@
 using System;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ButtonController : MonoBehaviour
+public class ButtonController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
 {
     private Button mainButton;
 
     [SerializeField] private GameObject mainButtonUsedState;
     [SerializeField] private GameObject mainButtonUnusedState;
 
+    [SerializeField] private Image mainButtonUnusedImage;
+    [SerializeField] private Color disabledUnusedColor = Color.gray;
+
     private UnityAction mainButtonEvent;
+    private bool isInteractable = true;
+    private Color originalUnusedColor;
 
     void Awake()
     {
         mainButton = GetComponent<Button>();
+        if (mainButtonUnusedImage != null)
+        {
+            originalUnusedColor = mainButtonUnusedImage.color;
+        }
 
         mainButton.onClick.AddListener(HandleClick);
 
         SetButtonState(true);
+        SetInteractable(true);
     }
 
     private void HandleClick()
     {
+        if (!isInteractable)
+        {
+            return;
+        }
         mainButtonEvent?.Invoke();
     }
 
@@ -48,5 +63,56 @@ public class ButtonController : MonoBehaviour
             mainButtonUnusedState.SetActive(false);
             mainButtonUsedState.SetActive(true);
         }
+    }
+
+    public void SetInteractable(bool canUse)
+    {
+        isInteractable = canUse;
+        if (mainButton != null)
+        {
+            mainButton.interactable = canUse;
+        }
+
+        if (mainButtonUnusedImage == null)
+        {
+            return;
+        }
+
+        if (canUse)
+        {
+            mainButtonUnusedImage.color = originalUnusedColor;
+        }
+        else
+        {
+            SetButtonState(true);
+            mainButtonUnusedImage.color = disabledUnusedColor;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!isInteractable)
+        {
+            return;
+        }
+        SetButtonState(false);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (!isInteractable)
+        {
+            return;
+        }
+        SetButtonState(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (!isInteractable)
+        {
+            return;
+        }
+        SetButtonState(true);
     }
 }
