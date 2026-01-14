8698fc92dbde954ad6ad7d17030159f69b01333c
Abdullah Akyol <abdullah35akyol@gmail.com>
2026-01-14 21:08:20 +0300

    FEAT: PlayerPrefsKeys

---
+++ Assets/Scripts/PlayerPrefsKeys.cs
@@ -0,0 +1,6 @@
+
+public static class PlayerPrefsKeys
+{
+    public const string LAST_EXIT_TIME = "OfflineReward_LastExitTime";
+    public const string HAS_SAVED_DATA = "OfflineReward_HasSavedData";
+}
